987a50faf1cad549ff35ec4c971e131e4f7ad52e
M0narX <si01082001@gmail.com>
2023-04-08 21:32:09 +0300

    add Views pages

--- ViewModels/Base/ViewModel.cs
+++ ViewModels/Base/ViewModel.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using JetBrains.Annotations;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CryptocurrencyApp.ViewModels.Base
 {
-    internal class ViewModel
+    internal abstract class ViewModel
     {
+        public bool HasChanges { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public virtual void Update() { }
+
+        public virtual void Dispose()
+        {
+
+        }
     }
 }
