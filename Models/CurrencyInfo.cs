200c842252e8ba36cd86ec56756e360cc8cde4ae
M0narX <si01082001@gmail.com>
2023-04-10 23:40:23 +0300

    add main models

---
+++ Models/CurrencyInfo.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptocurrencyApp.Models
+{
+    internal class CurrencyInfo
+    {
+        public CurrencyInfo(string currency)
+        {
+            string ResponseString = "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.coincap.io/v2/assets"+currency);
+            request.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            var stream = response.GetResponseStream();
+            if (stream != null)
+                ResponseString = new StreamReader(stream).ReadToEnd();
+            var json = JObject.Parse(ResponseString)["data"];
+            Name = currency;
+            Price = (float)json["supply"];
+            VolumeUsd24Hr = (float)json["volumeUsd24Hr"];
+            ChangePercent24Hr = (float)json["changePercent24Hr"];
+        }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public float VolumeUsd24Hr { get; set; }
+        public float ChangePercent24Hr { get; set; }
+    }
+}
---
+++ Models/CurrencyTopList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.IO;
+
+namespace CryptocurrencyApp.Models
+{
+    internal class CurrencyTopList
+    {
+        public CurrencyTopList()
+        {
+            string ResponseString = "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.coincap.io/v2/assets");
+            request.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            var stream = response.GetResponseStream();           
+            if (stream != null) 
+                ResponseString = new StreamReader(stream).ReadToEnd();
+            var json = JObject.Parse(ResponseString)["data"];
+            for (int i = 0; i < 10; i++)
+            {
+                CurrencyList.Add((string)json[i]["id"]);
+            }
+        }
+        public List<string> CurrencyList { get; set; }
+
+    }
+}
